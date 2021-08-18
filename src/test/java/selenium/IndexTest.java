82d1172f5b12a86addc2c26340a63ee7273de1ee
andrego <andre.gomes@duxus.com.br>
2021-08-18 18:48:15 -0300

    Configuração do selenium adicionada

---
+++ src/test/java/selenium/IndexTest.java
@@ -0,0 +1,24 @@
+package selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IndexTest {
+
+    public static void main(String[] args) throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://www.google.com/");
+        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
+
+    }
+}
