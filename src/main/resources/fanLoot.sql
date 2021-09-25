b2046f86be9b3d56988c073e9576bdab6908a11a
Ttallita <tallitalopesdesouza@gmail.com>
2021-09-25 15:04:09 -0300

    Atualizando banco e criando inserts

--- src/main/resources/fanLoot.sql
+++ src/main/resources/fanLoot.sql
@@ -1,29 +1,28 @@
+DROP TABLE IF EXISTS "usuarios" CASCADE;
 CREATE TABLE usuarios (
     usr_id serial NOT NULL,
     usr_email varchar(255) NOT NULL,
     usr_senha varchar(255) NOT NULL,
     usr_tipo varchar(255) NOT NULL,
-    usr_ativo boolean NOT NULL,
-
-    PRIMARY KEY (usr_id)
+    usr_ativo boolean NOT NULL
 );
 
+DROP TABLE IF EXISTS "clientes" CASCADE;
 CREATE TABLE clientes (
     cli_usr_id serial NOT NULL,
-    cli_prim_nome varchar(15) NOT NULL,
-    cli_ult_nome varchar(45) NOT NULL,
+    cli_car_id integer NOT NULL,
+    cli_nome varchar(15) NOT NULL,
+    cli_sobrenome varchar(45) NOT NULL,
     cli_cpf varchar(14) NOT NULL,
     cli_dt_nasc date NOT NULL,
     cli_genero varchar(10) NOT NULL,
     cli_telefone_num varchar(10) NOT NULL,
     cli_telefone_ddd varchar(2) NOT NULL,
     cli_telefone_tp varchar(11) NOT NULL,
-    cli_rank int,
-
-    PRIMARY KEY (cli_usr_id),
-    CONSTRAINT fk_cli_usr FOREIGN KEY (cli_usr_id) REFERENCES usuarios (usr_id)
+    cli_rank int
 );
 
+DROP TABLE IF EXISTS "cartoes" CASCADE;
 CREATE TABLE cartoes(
 	crt_id serial NOT NULL,
     crt_cli_usr_id int NOT NULL,
@@ -31,21 +30,20 @@ CREATE TABLE cartoes(
 	crt_bandeira varchar(255) NOT NULL,
 	crt_nome_impresso varchar(255) NOT NULL,
 	crt_dt_validade date NOT NULL,
-	crt_cod_seg varchar(3) NOT NULL,
-
-    PRIMARY KEY (crt_id),
-    CONSTRAINT fk_crt_cli FOREIGN KEY (crt_cli_usr_id) REFERENCES clientes (cli_usr_id)
+	crt_cod_seg varchar(3) NOT NULL
 );
 
+DROP TABLE IF EXISTS "cupons" CASCADE;
 CREATE TABLE cupons (
     cpm_id serial NOT NULL,
+    cpm_cli_usr_id integer,
+    cpm_vnd_id integer,
     cpm_tp varchar(11) NOT NULL,
     cpm_valor numeric(8, 2) NOT NULL,
-    cpm_descricao varchar(255),
-
-    PRIMARY KEY (cpm_id)
+    cpm_descricao varchar(255)
 );
 
+DROP TABLE IF EXISTS "enderecos" CASCADE;
 CREATE TABLE enderecos (
     end_id serial NOT NULL,
     end_cli_usr_id int NOT NULL,
@@ -59,117 +57,276 @@ CREATE TABLE enderecos (
 	end_cidade varchar(255) NOT NULL,
 	end_estado varchar(255) NOT NULL,
 	end_pais varchar(255) NOT NULL,
-	end_observacao varchar(255),
-
-    PRIMARY KEY (end_id),
-    CONSTRAINT fk_end_cli FOREIGN KEY (end_cli_usr_id) REFERENCES clientes (cli_usr_id)
-
+	end_observacao varchar(255)
 );
+
+DROP TABLE IF EXISTS "categorias" CASCADE;
 CREATE TABLE categorias(
     ctg_id serial NOT NULL,
-    ctg_nome varchar(255) NOT NULL,
-
-    PRIMARY KEY (ctg_id)
+    ctg_nome varchar(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS "fabricantes" CASCADE;
 CREATE TABLE fabricantes(
     fab_id serial NOT NULL,
-    fab_nome varchar(255) NOT NULL,
-
-    PRIMARY KEY (fab_id)
+    fab_nome varchar(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS "grupos_precificacao" CASCADE;
 CREATE TABLE grupos_precificacao(
     grp_id serial NOT NULL,
     grp_nome varchar(255) NOT NULL,
-    grp_porcentagem numeric(8,2) NOT NULL,
-    grp_margem_lucro numeric(8,2) NOT NULL,
-
-    PRIMARY KEY(grp_id)
+    grp_margem_lucro numeric(8,2) NOT NULL
 );
 
+DROP TABLE IF EXISTS "produtos" CASCADE;
 CREATE TABLE produtos(
---Todo produto que for inativado manualmente deve ter uma justificativa e uma categoria de inativação associada.
 	pro_id serial NOT NULL,
-    pro_ctg_id int NOT NULL,
     pro_fab_id int NOT NULL,
     pro_grp_id int NOT NULL,
+    pro_pst_id int NOT NULL,
 	pro_nome varchar(255) NOT NULL,
-	pro_valor_compra numeric(8, 2) NOT NULL, --NOT NULL??????????
-	pro_valor_venda numeric(8, 2) NOT NULL,
+	pro_valor_compra numeric(8, 2) NOT NULL,
+	pro_valor_venda numeric(8, 2),
     pro_quant_estoque int NOT NULL,
+    pro_limite_venda int,
 	pro_descricao varchar(255) NOT NULL,
 	pro_material varchar(255) NOT NULL,
     pro_cod_barras varchar(255) NOT NULL,
     pro_link varchar(255) NOT NULL,
-
-    PRIMARY KEY (pro_id),
-    CONSTRAINT fk_pro_fab FOREIGN KEY (pro_fab_id) REFERENCES fabricantes (fab_id),
-    CONSTRAINT fk_pro_grp FOREIGN KEY (pro_grp_id) REFERENCES grupos_precificacao (grp_id)
+    pro_dt_entrada_estoque date NOT NULL,
+    pro_limitado boolean NOT NULL
 );
 
+DROP TABLE IF EXISTS "categorias_produtos" CASCADE;
 CREATE TABLE categorias_produtos(
     ctp_pro_id int NOT NULL,
-    ctp_ctg_id int NOT NULL,
-
-    PRIMARY KEY (ctp_pro_id, ctp_ctg_id),
-    CONSTRAINT fk_ctp_pro FOREIGN KEY (ctp_pro_id) REFERENCES produtos (pro_id),
-    CONSTRAINT fk_ctp_ctg FOREIGN KEY (ctp_ctg_id) REFERENCES categorias (ctg_id)
+    ctp_ctg_id int NOT NULL
 );
 
-CREATE TABLE produtos_estados(
-    cti_id serial NOT NULL,
-    cti_tipo varchar(7),
-    cti_justificativa varchar(255),
-
-    PRIMARY KEY (cti_id)
+DROP TABLE IF EXISTS "produtos_status" CASCADE;
+CREATE TABLE produtos_status(
+    pst_id serial NOT NULL,
+    pst_tipo varchar(7) NOT NULL,
+    pst_categoria varchar(255) NOT NULL,
+    pst_justificativa varchar(255)
 );
 
+DROP TABLE IF EXISTS "carrinhos" CASCADE;
 CREATE TABLE carrinhos(
-    car_cli_id serial NOT NULL,
-    car_entrega_end_id int NOT NULL,
-    car_preco_total numeric(8, 2) NOT NULL,
-    car_frete numeric(8, 2) NOT NULL,
-
-    PRIMARY KEY (car_cli_id),
-    CONSTRAINT fk_car_cli FOREIGN KEY (car_cli_id) REFERENCES clientes (cli_usr_id)
+    car_cli_usr_id int NOT NULL,
+    --car_end_entrega_id int NOT NULL,
+    --car_preco_total numeric(8, 2) NOT NULL,
+    --car_frete numeric(8, 2) NOT NULL,
+    car_prazo_finalizacao timestamp
 );
 
+DROP TABLE IF EXISTS "produtos_em_carrinho" CASCADE;
 CREATE TABLE produtos_em_carrinho(
     prc_pro_id int NOT NULL,
-    prc_car_cli_id int NOT NULL,
-    prc_quant int NOT NULL,
-
-    PRIMARY KEY (prc_pro_id, prc_car_cli_id),
-    CONSTRAINT fk_prc_pro FOREIGN KEY (prc_pro_id) REFERENCES produtos (pro_id),
-    CONSTRAINT fk_prc_car FOREIGN KEY (prc_car_cli_id) REFERENCES carrinhos (car_cli_id)
+    prc_car_cli_usr_id int NOT NULL,
+    prc_quant int NOT NULL
 );
 
+DROP TABLE IF EXISTS "vendas" CASCADE;
 CREATE TABLE vendas (
 	vnd_id serial NOT NULL,
     vnd_cli_usr_id int NOT NULL,
-    vnd_crt_id int NOT NULL,
-    vnd_entrega_end_id int NOT NULL,
+    vnd_end_entrega_id int NOT NULL,
 	vnd_preco_total numeric(8,2) NOT NULL,
 	vnd_frete numeric(8,2) NOT NULL,
 	vnd_dt_compra timestamp NOT NULL,
     vnd_dt_envio timestamp,
     vnd_dt_entrega timestamp,
 	vnd_pagamento_aprovado boolean NOT NULL,
-	vnd_status_compra varchar(16) NOT NULL,
-
-    PRIMARY KEY (vnd_id),
-    CONSTRAINT fk_vnd_cli FOREIGN KEY (vnd_cli_usr_id) REFERENCES clientes (cli_usr_id),
-    CONSTRAINT fk_vnd_crt FOREIGN KEY (vnd_crt_id) REFERENCES cartoes (crt_id),
-    CONSTRAINT fk_vnd_end_entrega FOREIGN KEY (vnd_entrega_end_id) REFERENCES enderecos (end_id)
+	vnd_status_compra varchar(16) NOT NULL
 );
 
+DROP TABLE IF EXISTS "cartoes_em_venda" CASCADE;
+CREATE TABLE cartoes_em_venda (
+	crv_vnd_id int NOT NULL,
+    crv_crt_id int NOT NULL
+);
+
+DROP TABLE IF EXISTS "produtos_em_venda" CASCADE;
 CREATE TABLE produtos_em_venda(
     prv_pro_id int NOT NULL,
     prv_vnd_id int NOT NULL,
-    prv_quant int NOT NULL,
-
-    PRIMARY KEY (prv_pro_id, prv_vnd_id),
-    CONSTRAINT fk_prv_pro FOREIGN KEY (prv_pro_id) REFERENCES produtos (pro_id),
-    CONSTRAINT fk_prv_vnd FOREIGN KEY (prv_vnd_id) REFERENCES vendas (vnd_id)
+    prv_quant int NOT NULL
 );
+
+---
+
+DROP TABLE IF EXISTS "paises" CASCADE;
+CREATE TABLE paises (
+    pai_id bigserial NOT NULL,
+    pai_nome varchar(60),
+    pai_sigla varchar(2),
+    pai_bacen integer
+);
+
+DROP TABLE IF EXISTS "estados" CASCADE;
+CREATE TABLE estados (
+    etd_id bigserial NOT NULL,
+    etd_pai_id integer,
+    etd_nome varchar(60),
+    etd_uf varchar(2),
+    etd_ibge integer,
+    etd_ddd json
+);
+
+DROP TABLE IF EXISTS "municipios" CASCADE;
+CREATE TABLE municipios (
+    mnc_id bigserial NOT NULL,
+    mnc_etd_id integer NOT NULL,
+    mnc_nome varchar(255) NOT NULL,
+    mnc_cod_ibge integer NOT NULL
+);
+
+-- Constraints de primary key
+ALTER TABLE usuarios
+    ADD CONSTRAINT pk_usr PRIMARY KEY (usr_id);
+
+ALTER TABLE clientes
+    ADD CONSTRAINT pk_cli PRIMARY KEY (cli_usr_id);
+
+ALTER TABLE cartoes
+    ADD CONSTRAINT pk_crt PRIMARY KEY (crt_id);
+
+ALTER TABLE cupons
+    ADD CONSTRAINT pk_cpm PRIMARY KEY (cpm_id);
+
+ALTER TABLE enderecos
+    ADD CONSTRAINT pk_end PRIMARY KEY (end_id);
+
+ALTER TABLE categorias
+    ADD CONSTRAINT pk_ctg PRIMARY KEY (ctg_id);
+
+ALTER TABLE fabricantes
+    ADD CONSTRAINT pk_fab PRIMARY KEY (fab_id);
+
+ALTER TABLE grupos_precificacao
+    ADD CONSTRAINT pk_grp PRIMARY KEY(grp_id);
+
+ALTER TABLE produtos
+    ADD CONSTRAINT pk_pro PRIMARY KEY (pro_id);
+
+ALTER TABLE categorias_produtos
+    ADD CONSTRAINT pk_ctp PRIMARY KEY (ctp_pro_id, ctp_ctg_id);
+
+ALTER TABLE produtos_status
+    ADD CONSTRAINT pk_pst PRIMARY KEY (pst_id);
+
+ALTER TABLE carrinhos
+    ADD CONSTRAINT pk_car PRIMARY KEY (car_cli_usr_id);
+
+ALTER TABLE produtos_em_carrinho
+    ADD CONSTRAINT pk_prc PRIMARY KEY (prc_pro_id, prc_car_cli_usr_id);
+
+ALTER TABLE vendas
+    ADD CONSTRAINT pk_vnd PRIMARY KEY (vnd_id);
+
+ALTER TABLE cartoes_em_venda
+    ADD CONSTRAINT pk_crv PRIMARY KEY (crv_vnd_id, crv_crt_id);
+
+ALTER TABLE produtos_em_venda
+    ADD CONSTRAINT pk_prv PRIMARY KEY (prv_pro_id, prv_vnd_id);
+
+ALTER TABLE paises
+    ADD CONSTRAINT pk_pai PRIMARY KEY (pai_id);
+
+ALTER TABLE estados
+    ADD CONSTRAINT pk_etd PRIMARY KEY (etd_id);
+
+ALTER TABLE municipios
+    ADD CONSTRAINT pk_mnc PRIMARY KEY (mnc_id);
+
+
+-- Constraints de foreign key
+ALTER TABLE clientes
+    ADD CONSTRAINT fk_cli_usr FOREIGN KEY (cli_usr_id)
+        REFERENCES usuarios (usr_id);
+
+ALTER TABLE clientes
+    ADD CONSTRAINT fk_cli_car FOREIGN KEY (cli_car_id)
+        REFERENCES carrinhos (car_cli_usr_id);
+
+ALTER TABLE cartoes
+    ADD CONSTRAINT fk_crt_cli_usr FOREIGN KEY (crt_cli_usr_id)
+        REFERENCES clientes (cli_usr_id);
+
+ALTER TABLE cupons
+    ADD CONSTRAINT fk_cpm_cli_usr FOREIGN KEY (cpm_cli_usr_id)
+        REFERENCES clientes (cli_usr_id);
+
+ALTER TABLE cupons
+    ADD CONSTRAINT fk_cpm_vnd FOREIGN KEY (cpm_vnd_id)
+        REFERENCES vendas (vnd_id);
+
+ALTER TABLE enderecos
+    ADD CONSTRAINT fk_end_cli_usr FOREIGN KEY (end_cli_usr_id)
+        REFERENCES clientes (cli_usr_id);
+
+ALTER TABLE produtos
+    ADD CONSTRAINT fk_pro_fab FOREIGN KEY (pro_fab_id)
+        REFERENCES fabricantes (fab_id);
+
+ALTER TABLE produtos
+    ADD CONSTRAINT fk_pro_grp FOREIGN KEY (pro_grp_id)
+        REFERENCES grupos_precificacao (grp_id);
+
+ALTER TABLE produtos
+    ADD CONSTRAINT fk_pro_pst FOREIGN KEY (pro_pst_id)
+        REFERENCES produtos_status (pst_id);
+
+ALTER TABLE categorias_produtos
+    ADD CONSTRAINT fk_ctp_pro FOREIGN KEY (ctp_pro_id)
+        REFERENCES produtos (pro_id);
+
+ALTER TABLE categorias_produtos
+    ADD CONSTRAINT fk_ctp_ctg FOREIGN KEY (ctp_ctg_id)
+        REFERENCES categorias (ctg_id);
+
+ALTER TABLE carrinhos
+    ADD CONSTRAINT fk_car_cli_usr FOREIGN KEY (car_cli_usr_id)
+        REFERENCES clientes (cli_usr_id);
+
+ALTER TABLE produtos_em_carrinho
+    ADD CONSTRAINT fk_prc_pro FOREIGN KEY (prc_pro_id)
+        REFERENCES produtos (pro_id);
+
+ALTER TABLE produtos_em_carrinho
+    ADD CONSTRAINT fk_prc_car FOREIGN KEY (prc_car_cli_usr_id)
+        REFERENCES carrinhos (car_cli_usr_id);
+
+ALTER TABLE vendas
+    ADD CONSTRAINT fk_vnd_cli_usr FOREIGN KEY (vnd_cli_usr_id)
+        REFERENCES clientes (cli_usr_id);
+
+ALTER TABLE vendas
+    ADD CONSTRAINT fk_vnd_end_entrega FOREIGN KEY (vnd_end_entrega_id)
+        REFERENCES enderecos (end_id);
+
+ALTER TABLE cartoes_em_venda
+    ADD CONSTRAINT fk_crv_vnd FOREIGN KEY (crv_vnd_id)
+        REFERENCES vendas (vnd_id);
+
+ALTER TABLE cartoes_em_venda
+    ADD CONSTRAINT fk_crv_crt FOREIGN KEY (crv_crt_id)
+        REFERENCES cartoes (crt_id);
+
+ALTER TABLE produtos_em_venda
+    ADD CONSTRAINT fk_prv_pro FOREIGN KEY (prv_pro_id)
+        REFERENCES produtos (pro_id);
+
+ALTER TABLE produtos_em_venda
+    ADD CONSTRAINT fk_prv_vnd FOREIGN KEY (prv_vnd_id)
+        REFERENCES vendas (vnd_id);
+
+ALTER TABLE estados
+    ADD CONSTRAINT fk_etd_pai FOREIGN KEY (etd_pai_id)
+        REFERENCES paises (pai_id);
+
+ALTER TABLE municipios
+    ADD CONSTRAINT fk_mnc_etd FOREIGN KEY (mnc_etd_id)
+        REFERENCES estados (etd_id);
--- src/main/resources/inserts.sql
+++ src/main/resources/inserts.sql
@@ -1,13 +1,87 @@
 --Cliente Teste
 insert into usuarios (usr_email, usr_senha, usr_tipo, usr_ativo)
-values ('userexample@gmail.com', 'Teste123!', 'CLIENTE', true);
+values ('userexample@gmail.com', 'eb99f5951ac9d0bab4c6806198e6a696c63321e3de4ab1cb87aadfd0399fc2a1170c3c98f3a1372a9ed5287aaa5e4bc85be18047e67e83770ace301b2d20cadf', 'CLIENTE', true);
 
-insert into clientes (cli_usr_id, cli_prim_nome, cli_ult_nome, cli_cpf, cli_dt_nasc, cli_genero, cli_telefone_num, cli_telefone_ddd, cli_telefone_tp)
-values (4,'Ruan', 'Campos', '876.160.110-12', '2000-06-14','Masculino','98909-1345','11','Celular');
+insert into clientes (cli_usr_id, cli_nome, cli_sobrenome, cli_cpf, cli_dt_nasc, cli_genero, cli_telefone_num, cli_telefone_ddd, cli_telefone_tp)
+values (1,'Ruan', 'Campos', '876.160.110-12', '2000-06-14','Masculino','98909-1345','11','Celular');
 
 insert into enderecos (end_cli_usr_id, end_tp, end_nome, end_tp_logradouro, end_logradouro, end_num, end_bairro, end_cep, end_cidade, end_estado, end_pais)
-values (4,'COBRANCA_ENTREGA', 'Minha Casa', 'Rua', 'Onze', '5115', 'São Francisco', '29273-746', 'Vitória', 'Espírito Santo', 'Brasil');
+values (1,'COBRANCA_ENTREGA', 'Minha Casa', 'Rua', 'Onze', '5115', 'São Francisco', '29273-746', 'Vitória', 'Espírito Santo', 'Brasil');
 
 --Administrador Teste
 insert into usuarios (usr_email, usr_senha, usr_tipo, usr_ativo)
-values ('admexample@gmail.com', 'Teste123!', 'ADMINSTRADOR', true);
+values ('admexample@gmail.com', 'eb99f5951ac9d0bab4c6806198e6a696c63321e3de4ab1cb87aadfd0399fc2a1170c3c98f3a1372a9ed5287aaa5e4bc85be18047e67e83770ace301b2d20cadf', 'ADMINISTRADOR', true);
+--Teste123!
+
+--Categorias
+INSERT INTO categorias (ctg_nome) VALUES
+    ('Nendoiroid'),
+    ('Pelúcia'),
+    ('Action figure'),
+    ('Poster'),
+    ('Chaveiro');
+
+-- Produtos Status
+INSERT INTO produtos_status(pst_tipo, pst_categoria) VALUES
+    ('ATIVADO', 'DENTRO DE MERCADO'),
+    ('INATIVADO', 'FORA DE MERCADO');
+
+-- Grupos precificação
+INSERT INTO grupos_precificacao (grp_nome, grp_margem_lucro) VALUES
+    ('A', 0.3),
+    ('B', 0.25),
+    ('C', 0.2),
+    ('D', 0.15),
+    ('E', 0.1),
+    ('F', 0.5);
+
+--Fabricantes
+INSERT INTO fabricantes (fab_nome) VALUES
+    ('Vitae Aliquet Nec Ltd'),
+    ('Ornare Sagittis Industries'),
+    ('Sit Amet LLC'),
+    ('Enim Suspendisse Corporation'),
+    ('Dolor Nulla Corp.'),
+    ('Dolor Associates'),
+    ('Cras Sed Industries'),
+    ('Etiam Limited'),
+    ('Facilisis Vitae Orci Corp.'),
+    ('Dictum Associates'),
+    ('Lectus PC'),
+    ('Sem Mollis Dui Corp.'),
+    ('Magna Sed Corporation'),
+    ('Lacinia Foundation'),
+    ('Phasellus At PC'),
+    ('Dictum Placerat Institute'),
+    ('Pellentesque Eget Inc.'),
+    ('Est Inc.'),
+    ('Eget Magna Suspendisse Corp.'),
+    ('Pede Ultrices Industries'),
+    ('Nisl Consulting'),
+    ('In Molestie Corp.'),
+    ('Venenatis Limited'),
+    ('Augue Id Corp.'),
+    ('Et Associates'),
+    ('Donec Egestas Aliquam PC'),
+    ('Elit Dictum Inc.'),
+    ('Quam Elementum LLC'),
+    ('Purus In Consulting'),
+    ('Pellentesque Massa Corporation'),
+    ('Mollis Duis Institute'),
+    ('Dapibus Ligula Foundation'),
+    ('Gravida Molestie PC'),
+    ('Sit Amet Metus Associates'),
+    ('Volutpat Nulla LLC');
+
+-- Produtos
+INSERT INTO produtos (pro_fab_id, pro_grp_id, pro_nome, pro_valor_compra, pro_valor_venda, pro_quant_estoque, pro_descricao, pro_material, pro_cod_barras, pro_link) VALUES
+    (30, 2, 'Hatsune Miku Vocaloid', 90.0, 300., 100, 'pro_descricao', 'pro_material', 'A-0010-Z', '/link'),
+    (5, 4, 'Rei Neon Genesis Evangelion', 100.0, 300., 100, 'pro_descricao', 'pro_material', 'A-0020-Z', '/link'),
+    (16, 5, 'Levi Ackerman Shingeki no Kyojin', 180.0, 300., 100, 'pro_descricao', 'pro_material', 'A-0030-Z', '/link'),
+    (16, 1, 'Katsuki Bakugou Tiger My Hero Academia', 0.0, 300., 100, 'pro_descricao', 'pro_material', 'A-0040-Z', '/link'),
+    (7, 4, 'pro_nome', 0.0, 300., 100, 'pro_descricao', 'pro_material', 'A-0050-Z', '/link'),
+    (25, 5, 'pro_nome', 0.0, 300., 100, 'pro_descricao', 'pro_material', 'A-0060-Z', '/link'),
+    (19, 3, 'pro_nome', 0.0, 300., 100, 'pro_descricao', 'pro_material', 'A-0070-Z', '/link'),
+    (19, 3, 'pro_nome', 0.0, 300., 100, 'pro_descricao', 'pro_material', 'A-0080-Z', '/link'),
+    (19, 3, 'pro_nome', 0.0, 300., 100, 'pro_descricao', 'pro_material', 'A-0090-Z', '/link');
+    
