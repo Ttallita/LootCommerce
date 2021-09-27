dcf8a2c0de96cdd2c610632164369fb9c0f48d98
Ttallita <tallitalopesdesouza@gmail.com>
2021-09-26 21:29:52 -0300

    Criando ProdutoDAO

---
+++ src/main/java/dao/ProdutoDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import model.EntidadeDominio;
+
+import java.util.List;
+
+public class ProdutoDAO implements IDAO{
+    @Override
+    public EntidadeDominio salvar(EntidadeDominio entidade) {
+        return null;
+    }
+
+    @Override
+    public EntidadeDominio atualizar(EntidadeDominio entidade) {
+        return null;
+    }
+
+    @Override
+    public EntidadeDominio deletar(EntidadeDominio entidade) {
+        return null;
+    }
+
+    @Override
+    public List<EntidadeDominio> listar(EntidadeDominio entidade, String operacao) {
+        return null;
+    }
+}
