7d59037ca468d0d91f30e237e8857cc36d172d43
Ttallita <tallitalopesdesouza@gmail.com>
2021-09-29 01:23:02 -0300

    Criando model Fabricante.java e getters e setters em ItemCarrinho.java

--- src/main/java/model/carrinho/ItemCarrinho.java
+++ src/main/java/model/carrinho/ItemCarrinho.java
@@ -5,4 +5,20 @@ import model.produto.Produto;
 public class ItemCarrinho {
     private Produto produto;
     private int quant;
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+    
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }
 }
